a436dac3f32b390a1d2251e32765606ffac97ee3
charmErin <charm179@naver.com>
2023-06-27 23:48:12 +0900

    entity c, insert test, select test

---
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+import com.example.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+}
